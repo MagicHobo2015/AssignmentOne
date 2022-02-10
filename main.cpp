2343eb1d1d5ba05ab638c8bae49034e3c40433fa
Joshua Land <57689601+MagicHobo2015@users.noreply.github.com>
2022-02-10 09:41:26 -0800

    added some descriptions and worked on readme

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*********************************************************************************************
+*               Simple Console App.                                                          *
+*   Author(s):                                                                               *
+*       Joshua Land,                                                                         *
+*                                                                                            *
+*   Description: This is a simple command line interpreter, that gets and runs user commands.*
+*                                                                                            *
+*********************************************************************************************/
+
+// Includes
+#include <iostream>
+
+using std::cout; using std::getline; using std::endl;
+
+int main() 
+{
+    cout << " Hello World!" << endl;
+    return 0;
+}
